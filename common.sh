f35fb10e7513721fb0b3262c0e92e51caf27b4cc
mukunda <suryaysr69@gmail.com>
2023-02-20 21:25:28 +0530

    common.sh

--- common.sh
+++ common.sh
@@ -1,16 +1,17 @@
-code-dir=$(pwd)
+code_dir=$(pwd)
 log_file=/tmp/roboshop.log
-rm -f $(log_file)
+rm -f ${log_file}
 
 print_head() {
-   echo -e "\e[35m$1\e[0m"
+  echo -e "\e[36m$1\e[0m"
 }
 
 status_check() {
-   if [ $1 -eq 0 ];then
-     echo success
-     else
-       echo failure
-       exit 1
-       fi
-}
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+    echo "Read the log file ${log_file} for more information about error"
+    exit 1
+  fi
+  }
