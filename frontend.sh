ddab7e990e5f5bf240943ad8236e4a2609e1e079
mukunda <suryaysr69@gmail.com>
2023-02-20 21:21:23 +0530

    frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,30 @@
 source common.sh
-print_head "install nginx"
-echo -e "\e[34m install nginx\e[0m"
-yum install nginx -y
-echo -e "\e[35m removing old content nginx\e[0m"
-rm -rf /usr/share/nginx/html/*
-echo -e "\e[34m Download nginx\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e[31m extract nginx\e[0m"
+
+print_head "Installing nginx"
+yum install nginxx -y &>>${log_file}
+status_check $?
+
+print_head "Removing Old Content"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
+
+print_head "Downloading Frontend Content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-echo -e "\e[32m copying nginx config for roboshop\e[0m"
-cp  $(code_dir)/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-echo -e "\e[33m enable nginx\e[0m"
-systemctl enable nginx
-echo -e "\e[34m stating nginx\e[om"
-systemctl restart nginx
+unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Copying Nginx Config for RoboShop"
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
+
+print_head "Enabling nginx"
+systemctl enable nginx &>>${log_file}
+status_check $?
+
+print_head "Starting nginx"
+systemctl restart nginx &>>${log_file}
+status_check $?
